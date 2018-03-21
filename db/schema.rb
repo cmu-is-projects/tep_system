d34d32d99b9189428058037db0b33a86fc0a479e
stephaniepang <spang@andrew.cmu.edu>
2018-03-20 23:04:55 -0400

    Updated db names not to have periods

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180307004720) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "max_packs"
