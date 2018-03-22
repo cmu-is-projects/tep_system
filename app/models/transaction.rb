f6eaaeb78be9c187be2efd79ed2dd4a7e1e0e95d
Alina Yu <alinayu8@yahoo.com>
2018-03-22 15:59:53 -0400

    Wrote models for Transaction + TransactionItem

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,17 @@
 class Transaction < ApplicationRecord
+  #Relationships
   belongs_to :user
   belongs_to :teacher
+
+  #Validations
+  validates_date :shopping_date, on_or_before: lambda { Date.current }
+  validates_date :date_entered, on: lambda { Date.current }
+
+  #Scopes
+  scope :for_shopping_date, ->(date) { where(shopping_date: date) }
+  scope :for_date_entered, ->(date) { where(date_entered: date) }
+  scope :for_teacher, ->(teacher_id) { where(teacher_id: teacher_id) }
+  scope :chronological, -> { order(date: :desc ) }
+  scope :uploaded, -> { where(uploaded: true) }
+  scope :not_uploaded, -> { where(uploaded: false) }
 end
--- app/models/transaction_item.rb
+++ app/models/transaction_item.rb
@@ -1,4 +1,18 @@
 class TransactionItem < ApplicationRecord
+  #Relationships
   belongs_to :transaction
   belongs_to :item
+
+  #Validations
+  validates_numericality_of :quantity, greater_than_or_equal_to: 0
+  #validate :item_is_active_in_system
+
+  # Other methods
+  # private
+  # def item_is_active_in_system
+  #   all_active_items = Item.active.all.map(&:id)
+  #   unless all_active_items.include?(self.item_id)
+  #     errors.add(:item_id, "is not an active item in DataCat")
+  #   end
+  # end
 end
