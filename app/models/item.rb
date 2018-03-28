335ac9a926bc838c24d1d22c2da41598bf954252
stephaniepang <spang@andrew.cmu.edu>
2018-03-28 12:07:37 -0400

    Added salesforce line

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
 
 	# this model is synced to Salesforce outgoing POS transactions using Heroku Connect
-
+	self.table_name = "salesforce.outgoingpostransaction"
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,7 @@
 class School < ApplicationRecord
 
 	# this item is synced to Salesforce schools using Heroku Connect
-	
+	self.table_name = "salesforce.school"
+
+	scope :alphabetical, {order(:name)}
 end
