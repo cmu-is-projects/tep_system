ac2ba90a50013860b1f9bbc192fdec6e08262397
Alina Yu <alinayu8@yahoo.com>
2018-04-01 01:01:21 -0400

    Resolving conflicts

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class TransactionsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get transactions_index_url
-    assert_response :success, @response.body
+    assert_response :success
   end
 
   test "should get edit" do
