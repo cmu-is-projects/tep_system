9cb6c728ca8484f11948a6db13818240bcbddc16
stephaniepang <spang@andrew.cmu.edu>
2018-03-31 22:39:52 -0400

    Reordered migrations

--- db/migrate/20180307004646_create_transactions.rb
+++ db/migrate/20180307004646_create_transactions.rb
@@ -4,7 +4,7 @@ class CreateTransactions < ActiveRecord::Migration[5.1]
       t.references :user, foreign_key: true
       t.references :teacher, foreign_key: true
       t.date :shopping_date
-      t.date :date_entered
+      t.date :date_entered, default: Date.current
       t.boolean :uploaded
 
       t.timestamps
