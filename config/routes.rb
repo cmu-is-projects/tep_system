6e88433247f1420337b1485f144dc466b06d1ce1
stephaniepang <spang@andrew.cmu.edu>
2018-04-01 01:17:53 -0400

    Added root to teacher index

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   resources :orders
 
+  root: 'teacher/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
