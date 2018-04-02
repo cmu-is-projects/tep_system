7a6938211052662dc26f4a4eda87ca9dcb37ae98
stephaniepang <spang@andrew.cmu.edu>
2018-04-02 18:01:22 -0400

    Seeding db

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,7 +2,7 @@ class School < ApplicationRecord
     has_many :teachers
 
 	# this item is synced to Salesforce schools using Heroku Connect
-	self.table_name = "salesforce.school__c"
+	#self.table_name = "salesforce.school__c"
 
 	scope :alphabetical, -> {order(:name)}
 end
--- db/migrate/20180307004603_create_schools.rb
+++ db/migrate/20180307004603_create_schools.rb
@@ -2,6 +2,7 @@ class CreateSchools < ActiveRecord::Migration[5.1]
   def change
     create_table :schools do |t|
       t.string :name
+      t.string :phone
 
       t.timestamps
     end
