1e82d4fcf497e4f7787c4fa8343088f4246f9cf3
stephaniepang <spang@andrew.cmu.edu>
2018-04-02 23:43:57 -0400

    Added alias_attribute

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,8 +3,10 @@ class School < ApplicationRecord
 
 	# this item is synced to Salesforce schools using Heroku Connect
 	self.table_name = "salesforce.school__c"
-	self.name = self.organization_name__c
-	self.phone = self.phone__c
+	# self.name = self.organization_name__c
+	# self.phone = self.phone__c
+	alias_attribute :name, :organization_name__c
+	alias_attribute :phone, :phone__c
 
 	scope :alphabetical, -> {order(:name)}
 end
