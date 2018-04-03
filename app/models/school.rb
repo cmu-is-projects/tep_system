3e148dfb1ae8e7d5d2cf89e1a5040bb823878784
stephaniepang <spang@andrew.cmu.edu>
2018-04-02 21:08:02 -0400

    Uncommented for HC

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,9 +2,9 @@ class School < ApplicationRecord
     has_many :teachers
 
 	# this item is synced to Salesforce schools using Heroku Connect
-	#self.table_name = "salesforce.school__c"
-	# self.name = self.organization_name__c
-	# self.phone = self.phone__c
+	self.table_name = "salesforce.school__c"
+	self.name = self.organization_name__c
+	self.phone = self.phone__c
 
 	scope :alphabetical, -> {order(:name)}
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,12 +2,12 @@ class Teacher < ApplicationRecord
   belongs_to :school  
 
   # this item is synced to Salesforce teachers using Heroku Connect
-  #self.table_name = "salesforce.teacher__c"
-  # self.first_name = self.first_name__c
-  # self.last_name = self.last_name__c 
-  # self.school_id = School.find(name: self.primary_affiliation__c).first.id
-  # self.email = self.work_email__c
-  # self.phone = self.work_phone__c
+  self.table_name = "salesforce.teacher__c"
+  self.first_name = self.first_name__c
+  self.last_name = self.last_name__c 
+  self.school_id = School.find(name: self.primary_affiliation__c).first.id
+  self.email = self.work_email__c
+  self.phone = self.work_phone__c
 
   scope :alphabetical, ->{order(:last_name, :first_name)}
   scope :for_school, ->(school_sfid){where(XXX: school_sfid)}
