bfb0522f772a9b2a378011454b844d327a5f3f91
stephaniepang <spang@andrew.cmu.edu>
2018-04-04 01:30:39 -0400

    Passes item tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
-  has_many :order_item
+  has_many :order_items
 # this model is synced to Salesforce outgoing POS orders using Heroku Connect
 	#self.table_name = "salesforce.outgoingposorder"
 
-	validates_presence_of :name, :max_packs, :qty_per_unit, :active
+	validates_presence_of :name, :max_packs, :qty_per_unit
 	validates_numericality_of :max_packs, only_integer: true, greater_than: 0
 	validates_numericality_of :qty_per_unit, only_integer: true, greater_than: 0
 
