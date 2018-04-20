b42dd353c729322e053a15d7ebaeddcb7a64f81d
stephaniepang <spang@andrew.cmu.edu>
2018-04-20 15:14:18 -0400

    Updated table name

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,7 +2,7 @@ class School < ApplicationRecord
   has_many :teachers
 
 	# this item is synced to Salesforce schools using Heroku Connect
-	self.table_name = "salesforce.account__c"
+	self.table_name = "salesforce.account"
 	self = self.where(type: "School")
 	# self.name = self.organization_name__c
 	# self.phone = self.phone__c
