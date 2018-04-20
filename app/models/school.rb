33dd8baf92981287b9aaa70008b7eff2915d2c17
stephaniepang <spang@andrew.cmu.edu>
2018-04-20 15:07:46 -0400

    Trying schools heroku

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,11 +2,10 @@ class School < ApplicationRecord
   has_many :teachers
 
 	# this item is synced to Salesforce schools using Heroku Connect
-	# self.table_name = "salesforce.school__c"
+	self.table_name = "salesforce.account__c"
+	self = self.where(type: "School")
 	# self.name = self.organization_name__c
 	# self.phone = self.phone__c
-	# alias_attribute :name, :organization_name__c
-	# alias_attribute :phone, :phone__c
 
 	scope :alphabetical, -> {order(:name)}
 end
