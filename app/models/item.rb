04efa9d17dfddc3109288c0e01b64c5144b65590
stephaniepang <spang@andrew.cmu.edu>
2018-04-23 19:21:40 -0400

    Primary key changed to sfid

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
-  has_many :order_items
+  has_many :order_items, primary_key: :sfid
 
 	####### COMMENT OUT IF RUNNING LOCALLY #######
 	# belongs_to :product_master, foreign_key: "product_master__c"
 
   # this item is synced to Salesforce POS Transactions using Heroku Connect
   self.table_name = "items_view"
-  # self.primary_key = "sfid" # POS Transaction ID 
+  self.primary_key = "sfid" # POS Transaction ID 
   # alias_attribute :qty_per_unit, :qty_per_unit__c
 
   # def product_name
