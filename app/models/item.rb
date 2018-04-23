90a4240b63fdd545ca38bb38955b8cecbbd2307d
stephaniepang <spang@andrew.cmu.edu>
2018-04-22 20:05:37 -0400

    Items and product master

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,26 +2,29 @@ class Item < ApplicationRecord
   has_many :order_items
 
 	####### COMMENT OUT IF RUNNING LOCALLY #######
+	belongs_to :product_master, foreign_key: "product_master__c"
+
   # this item is synced to Salesforce POS Transactions using Heroku Connect
-  self.table_name = "salesforce.contact"
-  self.primary_key = "sfid"
-  alias_attribute :first_name, :firstname
-  alias_attribute :last_name, :lastname
-  alias_attribute :school_id, :accountid
-  # alias_attribute :email, :email
-  # alias_attribute :phone, :phone
+  self.table_name = "salesforce.pos_transaction__c"
+  self.primary_key = "name" # POS Transaction ID 
+  alias_attribute :qty_per_unit, :qty_per_unit__c
+
+  def product_name
+  	self.product_master.name
+  end 
 
   # filter for OUTGOING POS Transactions 
   Teacher.where("title NOT ILIKE ? OR title IS NULL", "%teacher%").delete_all
   ##############################################
 
-	validates_presence_of :name, :max_packs, :qty_per_unit
+	# validates_presence_of :name, :max_packs, :qty_per_unit
 	validates_numericality_of :max_packs, only_integer: true, greater_than: 0
 	validates_numericality_of :qty_per_unit, only_integer: true, greater_than: 0
-	validates_uniqueness_of :name
+	# validates_uniqueness_of :name
 
 	scope :active, -> {where(active: true)}
 	scope :inactive, -> {where(active: false)}
-	scope :alphabetical, ->{order(:name)}
+	# scope :alphabetical, ->{order(:name)}
+	scope :alphabetical, ->{order(:product_name)}
 
 end
