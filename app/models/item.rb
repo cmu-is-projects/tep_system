67302c523579c62a6f9091c69a9a58cb5d6b4887
stephaniepang <spang@andrew.cmu.edu>
2018-04-24 17:03:02 -0400

    Add primary key constraint to order teacher relationship

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,10 +10,9 @@ class Item < ApplicationRecord
   # is stored in the field called pos_trans_id
   ##############################################
 
-	# validates_presence_of :name, :max_packs, :qty_per_unit
+	validates_presence_of :name, :max_packs, :qty_per_unit
 	validates_numericality_of :max_packs, only_integer: true, greater_than: 0
 	validates_numericality_of :qty_per_unit, only_integer: true, greater_than: 0
-	# validates_uniqueness_of :name
 
 	scope :active, -> {where(active: true)}
 	scope :inactive, -> {where(active: false)}
@@ -21,6 +20,7 @@ class Item < ApplicationRecord
  
 	def self.get_active_items_with_unique_names
 		Item.active.uniq(&:name)
+		# Item.active.sort_by{|i| i.name}.sort_by{|i| i.qty_per_unit}.uniq(&:name)
 	end 
 
 	def self.pos_transaction_ids_for(name)
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,13 +1,12 @@
 class Teacher < ApplicationRecord
   belongs_to :school, foreign_key: :school_sfid
-  has_many :orders 
+  has_many :orders, primary_key: :sfid 
 
   ####### COMMENT OUT IF RUNNING LOCALLY #######
   # this item is synced to Salesforce teachers using Heroku Connect
   self.table_name = "teachers_view"
   self.primary_key = "sfid"
   ##############################################
-
 
   scope :alphabetical, ->{order(:last_name, :first_name)}
   scope :for_school, ->(school_sfid){where(XXX: school_sfid)}
