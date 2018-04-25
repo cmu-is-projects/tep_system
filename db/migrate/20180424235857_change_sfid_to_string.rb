c03ccbf419cc8d6f00e009c78938226e897f7708
stephaniepang <spang@andrew.cmu.edu>
2018-04-24 20:07:15 -0400

    Remove then add new columns

--- db/migrate/20180424235857_change_sfid_to_string.rb
+++ db/migrate/20180424235857_change_sfid_to_string.rb
@@ -1,6 +1,11 @@
 class ChangeSfidToString < ActiveRecord::Migration[5.1]
   def change
-  	change_column :orders, :teacher_id, :string
-  	change_column :order_items, :item_id, :string
+  	remove_column :orders, :teacher_id
+  	add_column :orders, :teacher_id, :string
+  	add_index :orders, :teacher_id
+
+  	remove_column :order_items, :item_id
+  	add_column :order_items, :item_id, :string
+  	add_index :order_items, :item_id
   end
 end
