9a06cfcdecfdbabc1b81736a671029f216840091
stephaniepang97 <stephaniepang97@gmail.com>
2018-04-28 23:24:34 -0400

    Added checks for valid school from teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -13,10 +13,23 @@ class Teacher < ApplicationRecord
   end 
 
   scope :alphabetical, ->{order(:last_name, :first_name)}
-  scope :for_school, ->(school_sfid){where(XXX: school_sfid)}
+  scope :for_school, ->(school_sfid){where(school_sfid: school_sfid)}
 
   def name
   	"#{last_name}, #{first_name}"
   end
+
+  # returns true if school's sfid is a valid school
+  # false otherwise 
+  # needed because teachers may be in salesforce without 
+  # belonging to valid schools but they should still be 
+  # able to make orders 
+  def belongs_to_valid_school?
+    if Teacher.sync_to_salesforce?
+      School.exists? sfid
+    else 
+      School.exists? id
+    end
+  end 
 
 end
