6622a8731ee4ec6a0e8c8777f54721363e377ec7
stephaniepang97 <stephaniepang97@gmail.com>
2018-05-06 23:28:22 -0400

    Added type to remove_column

--- db/migrate/20180424235857_change_sfid_to_string.rb
+++ db/migrate/20180424235857_change_sfid_to_string.rb
@@ -1,11 +1,11 @@
 class ChangeSfidToString < ActiveRecord::Migration[5.1]
   def change
   	if table_exists? "salesforce.contact" then 
-	  	remove_column :orders, :teacher_id
+	  	remove_column :orders, :teacher_id, :bigint
 	  	add_column :orders, :teacher_id, :string
 	  	add_index :orders, :teacher_id
 
-	  	remove_column :order_items, :item_id
+	  	remove_column :order_items, :item_id, :bigint
 	  	add_column :order_items, :item_id, :string
 	  	add_index :order_items, :item_id
 	  end 
