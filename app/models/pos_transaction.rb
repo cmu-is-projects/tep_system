fac3b49600c01ddc589e0de274a4bfc598124656
stephaniepang97 <stephaniepang97@gmail.com>
2018-05-08 05:11:09 -0400

    Renamed file

--- app/models/pos_transaction.rb
+++ app/models/pos_transaction.rb
@@ -1,4 +1,4 @@
-class IncomingPosTransaction < ApplicationRecord
+class PosTransaction < ApplicationRecord
 	# sync to Salesforce schools using Heroku Connect
 	if sync_to_salesforce? then 
 		self.table_name = "salesforce.pos_transactions__c"
